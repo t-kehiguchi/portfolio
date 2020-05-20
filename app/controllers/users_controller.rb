14d8b8af1e5623ca92477f99672a4c6c0c05adbe
t-kehiguchi <ec2-user@ip-172-31-32-60.ap-northeast-1.compute.internal>
2020-05-20 15:00:06 +0000

    一般ユーザーはエンジニア登録／更新確認画面に遷移しないように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -161,6 +161,8 @@ class UsersController < ApplicationController
   end
 
   def confirm
+    invalidUrl()
+    return if response_body ## 一般ユーザーのみ強制リダイレクト
     ## 登録画面か更新画面からの遷移ではない場合は
     unless request.referer or params[:name].present?
       flash[:danger] = '登録画面か更新画面から遷移してください。'
