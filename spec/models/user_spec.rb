a3c1a99e7155043812567a951ee28e5c25c83ee2
t-kehiguchi <t0tp50gxgpb8ibksqp9r@outlook.com>
2021-06-28 00:00:40 +0900

    ローカルDBに依存せずに前提条件(before)でレコードReplaceするように修正(一部文言も修正)

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,12 +14,28 @@ RSpec.describe User, type: :model do
   end
 
   describe "scopeのengineerについて" do
-    context "DBに社員番号「99999999」はエンジニアではないという前提で" do
+    context "エンジニアではない場合" do
+      before "DBに社員番号「99999999」はエンジニアではないという前提で(仮に存在した場合はReplace(admin_flagが変更されていることも考慮して))" do
+        record = User.where(employee_number: 99999999)
+        record.destroy_all if record.present?
+        User.create!(employee_number: 99999999, password: "password", password_confirmation: "password",
+                     name: "A", department: "B", birthday: "2021-06-28",
+                     nearest_station: "C", telephone_number: "012-3456-7890", join_able_date: nil,
+                     admin_flag: User.admins['admin'])
+      end
       it "存在しない(0件である)こと" do
         expect(User.engineer.where(employee_number: 99999999).count).to eq 0
       end
     end
-    context "DBに社員番号「11111111」はエンジニアであるという前提で" do
+    context "エンジニアではある場合" do
+      before "DBに社員番号「11111111」はエンジニアであるという前提で(仮に存在した場合はReplace(admin_flagが変更されていることも考慮して)" do
+        record = User.where(employee_number: 11111111)
+        record.destroy_all if record.present?
+        User.create!(employee_number: 11111111, password: "password", password_confirmation: "password",
+                     name: "A", department: "B", birthday: "2021-06-28",
+                     nearest_station: "C", telephone_number: "012-3456-7890", join_able_date: "2021-06-28",
+                     admin_flag: User.admins['general'])
+      end
       it "存在する(1件である)こと" do
         expect(User.engineer.where(employee_number: 11111111).count).to eq 1
       end
