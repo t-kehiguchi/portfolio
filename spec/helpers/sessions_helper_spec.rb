134da159e1163fbadad1ed1ad0f5c313dcdf62f9
t-kehiguchi <t0tp50gxgpb8ibksqp9r@outlook.com>
2021-07-08 00:00:23 +0900

    RSpec修正(DBにユーザーが存在する場合はnewではなくfindでユーザー情報を取得するように)

--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -4,16 +4,16 @@ RSpec.describe SessionsHelper, type: :helper do
 
   describe "引数に渡されたユーザーでログインするメソッドについて" do
     it "正常にセッション情報に社員番号が格納されること" do
-      user = User.new(employee_number: 12345678)
+      user = User.find(11111111)
       helper.log_in(user)
-      expect(session[:employee_number]).to eq 12345678
+      expect(session[:employee_number]).to eq 11111111
     end
   end
 
   describe "現在のユーザーをログアウトするメソッドについて" do
     context "ログアウト前にログインされている状態で" do
       before do
-        user = User.new(employee_number: 12345678)
+        user = User.find(11111111)
         helper.log_in(user)
       end
       it "ログアウト後にセッション情報がnilとなること" do
@@ -25,7 +25,7 @@ RSpec.describe SessionsHelper, type: :helper do
 
   describe "現在ログイン中のユーザーを返す (ただし、いる場合のみ)メソッドについて" do
     before "DB(Userテーブル)に存在している社員番号「11111111」がログインしている前提で" do
-      user = User.new(employee_number: 11111111)
+      user = User.find(11111111)
       helper.log_in(user)
     end
     it "ログインユーザーは現在社員番号「11111111」のユーザーであること" do
@@ -40,7 +40,7 @@ RSpec.describe SessionsHelper, type: :helper do
   describe "ユーザーがログインしていればtrue、その他ならfalseを返すメソッドについて" do
     context "DB(Userテーブル)に存在している社員番号のユーザーがログイン中の場合" do
       before do
-        user = User.new(employee_number: 11111111)
+        user = User.find(11111111)
         helper.log_in(user)
       end
       it "trueが返却されること" do
@@ -58,7 +58,7 @@ RSpec.describe SessionsHelper, type: :helper do
     end
     context "ログイン直後にログアウトした場合" do
       before do
-        user = User.new(employee_number: 11111111)
+        user = User.find(11111111)
         helper.log_in(user)
         helper.log_out
       end
